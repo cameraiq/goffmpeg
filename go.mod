ae6fef1131262657e0b310a54e1ec1f568bd1e97
Frank Tinsley <franktinsley@me.com>
2020-06-01 14:59:34 -0700

    Import cameraiq fork

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/xfrr/goffmpeg
 
 go 1.14
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/cameraiq/goffmpeg v0.0.0-20191108092855-c49093f2d9f7
+	github.com/stretchr/testify v1.5.1
+)
--- transcoder/transcoder.go
+++ transcoder/transcoder.go
@@ -12,9 +12,9 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/xfrr/goffmpeg/ffmpeg"
-	"github.com/xfrr/goffmpeg/models"
-	"github.com/xfrr/goffmpeg/utils"
+	"github.com/cameraiq/goffmpeg/ffmpeg"
+	"github.com/cameraiq/goffmpeg/models"
+	"github.com/cameraiq/goffmpeg/utils"
 )
 
 // Transcoder Main struct
