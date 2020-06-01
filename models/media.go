a03f71899f1391982a633db5b70d3cb47de549b0
Frank Tinsley <franktinsley@me.com>
2020-06-01 15:58:51 -0700

    Revert "Fix errors"
    This reverts commit 354f5ad96fc0151fce51b075188acea83df3a2a5.

--- models/media.go
+++ models/media.go
@@ -823,7 +823,7 @@ func (m *Mediafile) ObtainVframes() []string {
 
 func (m *Mediafile) ObtainVsync() []string {
 	if m.vsync != "" {
-		return []string{"-vsync", fmt.Sprintf("%s", m.vsync)}
+		return []string{"-vsync", fmt.Sprintf("%s", m.vframes)}
 	}
 	return nil
 }
@@ -1131,7 +1131,7 @@ func (m *Mediafile) ObtainSkipAudio() []string {
 
 func (m *Mediafile) ObtainStreamIds() []string {
 	if m.streamIds != nil && len(m.streamIds) != 0 {
-		var result []string
+		result := []string{}
 		for i, val := range m.streamIds {
 			result = append(result, []string{"-streamid", fmt.Sprintf("%d:%s", i, val)}...)
 		}
@@ -1167,7 +1167,7 @@ func (m *Mediafile) ObtainBframe() []string {
 
 func (m *Mediafile) ObtainTags() []string {
 	if m.tags != nil && len(m.tags) != 0 {
-		var result []string
+		result := []string{}
 		for key, val := range m.tags {
 			result = append(result, []string{"-metadata", fmt.Sprintf("%s=%s", key, val)}...)
 		}
